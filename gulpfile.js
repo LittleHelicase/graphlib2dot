cedd1970fd8da3d7a9150e5ebeeb0460487fe114
Maximilian Klein <dasmaxl@gmail.com>
2016-01-06 23:39:17 +0100

    (feature) Add back conversion.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp')
 var babel = require('gulp-babel')
+var es = require('event-stream')
 
 gulp.task('babel', function () {
-  return gulp.src('index.js')
+  return es.concat(
+    gulp.src('index.js')
       .pipe(babel())
-      .pipe(gulp.dest('lib'))
+      .pipe(gulp.dest('lib')),
+    gulp.src('index_back.js')
+      .pipe(babel())
+      .pipe(gulp.dest('lib')))
 })
 
 gulp.task('default', ['babel'])
